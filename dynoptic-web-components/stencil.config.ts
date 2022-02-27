3a4a11c91c80d167e2211f564bb016eaea8e2c1b
Viktor Danko <vik@panter.ch>
2022-02-27 18:02:01 +0100

    stuff

--- dynoptic-web-components/stencil.config.ts
+++ dynoptic-web-components/stencil.config.ts
@@ -11,16 +11,20 @@ export const config: Config = {
       includeDefineCustomElements: true,
     }),
     {
+      //  generate the component(s) as a reusable library
       type: 'dist',
       esmLoaderPath: '../loader',
     },
     {
+      // generate custom elements in a more optimized way for tree shaking
       type: 'dist-custom-elements',
     },
     {
+      // auto-generate readme.md files in markdown
       type: 'docs-readme',
     },
     {
+      // oriented for webapps and websites, hosted from an http server
       type: 'www',
       serviceWorker: null, // disable service workers
     },
